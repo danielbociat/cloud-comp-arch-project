6f05ee58efee41fa68f7e77afb8437e6078f3df2
danielbociat <danibociat@gmail.com>
2025-04-30 21:18:00 +0200

    Added infra for part3

--- part3.py
+++ part3.py
@@ -22,7 +22,7 @@ config = {
     "freqmine" : [nodes[0],1],
     "radix" : [nodes[0],1],
     "vips" : [nodes[0],1],
-    "memecached" : [nodes[0], 1]
+    "memcached" : [nodes[0], 1]
 }
 
 NUM_RUNS = 1
@@ -69,7 +69,7 @@ def write_run_data(filename, no_threads, job_name, real_time, user_time, sys_tim
     with open(filename, "a") as f:
         f.write(line)
 
-def update_template(filename, node_label, num_threads):
+def update_template(filename, node_label, num_threads, cores = "0"):
     script_dir = os.path.dirname(__file__)
     rel_path = f"part3/{filename}-template.yaml"
     abs_file_path = os.path.join(script_dir, rel_path)
@@ -79,7 +79,7 @@ def update_template(filename, node_label, num_threads):
 
     schemas = schemas.replace("<n>", str(num_threads))
     schemas = schemas.replace("<node_label>", node_label)
-    schemas = schemas.replace("<cores>", "0")
+    schemas = schemas.replace("<cores>", cores)
 
     script_dir = os.path.dirname(__file__)
     rel_path = f"part3/{filename}.yaml"
@@ -102,25 +102,81 @@ if __name__ == '__main__':
     lines = output.strip().split("\n")
     for line in lines[1:]:
         if "client-agent-a-" in line:
+            client_agent_a_internal_ip = line.split()[5]
             client_agent_a = line.split()[0]
         if "client-agent-b-" in line:
+            client_agent_b_internal_ip = line.split()[5]
             client_agent_b = line.split()[0]
         if "client-measure" in line:
             client_measure = line.split()[0]
+
+    print("A: ", client_agent_a, client_agent_a_internal_ip)
+    print("B: ", client_agent_b, client_agent_b_internal_ip)
 
     current_time = datetime.now()
     formatted_time = current_time.strftime("%d-%m-%Y-%H-%M")    
     
     for job in jobs:
         update_template(job, config[job][0], config[job][1])
-    update_template("memecached", config["memecached"][0], config["memecached"][1])
+    update_template("memcached", config["memcached"][0], 2, "0,1")
 
-    subprocess.run(["glcoud", "compute", "scp", "update_mcperf.sh",  f"ubuntu@{client_agent_a}:~/", "--zone", "europe-west1-b", 
+    subprocess.run(["kubectl", "create", "-f", f"part3/memcached.yaml"], env=env, check=True)
+    subprocess.run(["kubectl", "expose", "pod", "some-memcached", "--name", "some-memcached-11211",
+                    "--type", "LoadBalancer", "--port", "11211", "--protocol", "TCP"], env=env, check=True)
+
+    output = subprocess.check_output(["kubectl", "get", "pods", "--selector=name=some-memcached", "-o", "wide"], env=env, text=True)
+    while not is_pod_ready(output):
+        print("Memcached not ready yet...")
+        time.sleep(30)
+        output = subprocess.check_output(["kubectl", "get", "pods", "--selector=name=some-memcached", "-o", "wide"], env=env, text=True)
+    
+    lines = output.strip().split("\n")
+    for line in lines:
+        print(line)
+        if "memcache" in line:
+            memcached_ip = line.split()[5]
+    print("MEMCACHED_IP: ", memcached_ip)
+
+    print("Memcached running!")
+
+    subprocess.run(["gcloud", "compute", "scp", "update_mcperf.sh",  f"ubuntu@{client_agent_a}:~/", "--zone", "europe-west1-b", 
                     "--ssh-key-file", "~/.ssh/cloud-computing"])
-    subprocess.run(["glcoud", "compute", "ssh", f"ubuntu@{client_agent_a}", "--zone", "europe-west1-b", 
+    print("Uploaded shell script to client A")
+
+    subprocess.run(["gcloud", "compute", "ssh", f"ubuntu@{client_agent_a}", "--zone", "europe-west1-b", 
                     "--ssh-key-file", "~/.ssh/cloud-computing", "--command", "chmod +x ~/update_mcperf.sh && ~/update_mcperf.sh"])
-    subprocess.run(["glcoud", "compute", "ssh", f"ubuntu@{client_agent_a}", "--zone", "europe-west1-b", 
-                    "--ssh-key-file", "~/.ssh/cloud-computing", "--command", "./mcperf -T 2 -A"])
+    print("Updated mcperf in client A")
+
+    subprocess.Popen(["gcloud", "compute", "ssh", f"ubuntu@{client_agent_a}", "--zone", "europe-west1-b", 
+                    "--ssh-key-file", "~/.ssh/cloud-computing", "--quiet", "--command", "cd memcache-perf-dynamic && ./mcperf -T 2 -A"], stdout=subprocess.DEVNULL)
+    print("Started mcperf in client A")
+
+    subprocess.run(["gcloud", "compute", "scp", "update_mcperf.sh",  f"ubuntu@{client_agent_b}:~/", "--zone", "europe-west1-b", 
+                    "--ssh-key-file", "~/.ssh/cloud-computing"])
+    print("Uploaded shell script to client B")
+
+    subprocess.run(["gcloud", "compute", "ssh", f"ubuntu@{client_agent_b}", "--zone", "europe-west1-b", 
+                    "--ssh-key-file", "~/.ssh/cloud-computing", "--command", "chmod +x ~/update_mcperf.sh && ~/update_mcperf.sh"])
+    print("Updated mcperf in client B")
+    
+    subprocess.Popen(["gcloud", "compute", "ssh", f"ubuntu@{client_agent_b}", "--zone", "europe-west1-b", 
+                    "--ssh-key-file", "~/.ssh/cloud-computing", "--quiet", "--command", "cd memcache-perf-dynamic && ./mcperf -T 4 -A"], stdout=subprocess.DEVNULL)
+    print("Started mcperf in client B")
+
+    subprocess.run(["gcloud", "compute", "scp", "update_mcperf.sh",  f"ubuntu@{client_measure}:~/", "--zone", "europe-west1-b", 
+                    "--ssh-key-file", "~/.ssh/cloud-computing"])
+    print("Uploaded shell script to client Measure")
+
+    subprocess.run(["gcloud", "compute", "ssh", f"ubuntu@{client_measure}", "--zone", "europe-west1-b", 
+                    "--ssh-key-file", "~/.ssh/cloud-computing", "--command", "chmod +x ~/update_mcperf.sh && ~/update_mcperf.sh"])
+    print("Updated mcperf in client Measure")
+    
+    subprocess.run(["gcloud", "compute", "ssh", f"ubuntu@{client_measure}", "--zone", "europe-west1-b", 
+                    "--ssh-key-file", "~/.ssh/cloud-computing", "--command", f"cd memcache-perf-dynamic && ./mcperf -s {memcached_ip} --loadonly"])
+    subprocess.Popen(["gcloud", "compute", "ssh", f"ubuntu@{client_measure}", "--zone", "europe-west1-b", 
+                    "--ssh-key-file", "~/.ssh/cloud-computing", "--command", f"cd memcache-perf-dynamic && ./mcperf -s {memcached_ip} -a {client_agent_a_internal_ip} \
+                        -a {client_agent_b_internal_ip} --noload -T 6 -C 4 -D 4 -Q 1000 -c 4 -t 10 --scan 30000:30500:5 > results.txt"], stdout=subprocess.DEVNULL)
+    print("Started mcperf in client Measure")
 
     # # Run the processes
     # for i in range(NUM_RUNS):
@@ -134,6 +190,11 @@ if __name__ == '__main__':
     # # Make sure there are no witnesses
     # subprocess.run(["kubectl", "delete", "jobs", "--all"])
     # subprocess.run(["kubectl", "delete", "pods", "--all"])
+
+    time.sleep(120)
+
+    subprocess.run(["gcloud", "compute", "scp", f"ubuntu@{client_measure}:~/memcache-perf-dynamic/results.txt", "memcached_results.txt", "--zone", "europe-west1-b", 
+               "--ssh-key-file", "~/.ssh/cloud-computing"])
 
     # subprocess.run(["kops", "delete", "cluster", "--name", f"part3.k8s.local", "--yes"], check=True)
     # print("Successfully deleted cluster!")
