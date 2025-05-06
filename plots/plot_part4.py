5d548e6ebc08ba8187b228d2037b9f5818c163cf
cblack389 <negruclaudiu389@yahoo.com>
2025-05-06 14:59:38 +0200

    Use std dev instead of max dev, beautify plot

--- plots/plot_part4.py
+++ plots/plot_part4.py
@@ -1,7 +1,10 @@
 import os
+import math
+
 from pathlib import Path
 
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 
 MCPERF_OUTPUT_1A_PATH = Path("./data/part4/1a/")
@@ -24,7 +27,7 @@ class ResultData:
     def add_p95(self, run: int, val: float):
         self.p95[run].append(val)
 
-    def compute_averages_and_errors(self):
+    def compute_averages_and_errors(self, std_dev=True):
         num_points = len(self.qps[0])
         self.qps_averages = [0 for _ in range(num_points)]
         self.p95_averages = [0 for _ in range(num_points)]
@@ -38,16 +41,31 @@ class ResultData:
         self.qps_averages = [val / self.num_runs for val in self.qps_averages]
         self.p95_averages = [val / self.num_runs for val in self.p95_averages]
 
-        for run in range(self.num_runs):
-            for i in range(num_points):
-                qps_diff = abs(self.qps_averages[i] - self.qps[run][i])
-                if qps_diff > self.qps_errors[i]:
-                    self.qps_errors[i] = qps_diff
+        if not std_dev: # error is max deviation
+            for run in range(self.num_runs):
+                for i in range(num_points):
+                    qps_diff = abs(self.qps_averages[i] - self.qps[run][i])
+                    if qps_diff > self.qps_errors[i]:
+                        self.qps_errors[i] = qps_diff
 
-                p95_diff = abs(self.p95_averages[i] - self.p95[run][i])
-                if p95_diff > self.p95_errors[i]:
-                    self.p95_errors[i] = p95_diff
-        
+                    p95_diff = abs(self.p95_averages[i] - self.p95[run][i])
+                    if p95_diff > self.p95_errors[i]:
+                        self.p95_errors[i] = p95_diff
+        else:
+            for i in range(num_points):
+                qps_vals = [self.qps[run][i] for run in range(self.num_runs)]
+                p95_vals = [self.p95[run][i] for run in range(self.num_runs)]
+
+                qps_mean = self.qps_averages[i]
+                p95_mean = self.p95_averages[i]
+
+                qps_var = sum((val - qps_mean) ** 2 for val in qps_vals) / (self.num_runs - 1)
+                p95_var = sum((val - p95_mean) ** 2 for val in p95_vals) / (self.num_runs - 1)
+
+                self.qps_errors[i] = math.sqrt(qps_var)
+                self.p95_errors[i] = math.sqrt(p95_var)
+
+                    
     def get_plot_values(self) -> tuple:
         self.compute_averages_and_errors()
         return self.qps_averages, self.p95_averages, self.qps_errors, self.p95_errors
@@ -73,13 +91,14 @@ def parse_mcperf_output(output_path: Path) -> dict:
                     p95 = float(line_split[12])
                     qps = float(line_split[16])
                     results[(threads, cores)].add_qps(num_run, qps)
-                    results[(threads, cores)].add_p95(num_run, p95)
+                    results[(threads, cores)].add_p95(num_run, float(p95 / 1000))
 
     return results
 
 
 def plot_41a(results: dict):
     plt.figure(figsize=(14, 8))
+    ax = plt.gca()
 
     for k in results:
         x, y, xerr, yerr = results[k].get_plot_values()
@@ -87,11 +106,18 @@ def plot_41a(results: dict):
         cores_label = "1 core" if k[1] == 1 else "2 cores"
         plt.errorbar(x, y, yerr, xerr, capsize=4, elinewidth=1.5, markersize=4, label=f"{threads_label}, {cores_label}")
 
-    # plt.ylim(0, 1000)
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(10000))
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, _: f"{int(x / 1000)}k"))
+
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
+    ax.yaxis.set_major_formatter(ticker.FormatStrFormatter("%.1f"))
+
+    plt.xlim(0, 220000)
     plt.xlabel("QPS")
-    plt.ylabel("p95")
+    plt.ylabel("p95 (ms)", rotation=0, labelpad=40)
     plt.title("p95 vs QPS for Each Configuration\nAveraged over 3 runs")
     plt.grid(True)
+    plt.tight_layout()
     plt.legend()
     plt.show()
 
