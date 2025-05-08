8494acf01edd0c67dba8c4fd7cc3bff9de8df0b9
claudiu14c <claudiucraciun14c@gmail.com>
2025-05-08 13:34:52 +0200

    measure cou us=tilisation for each core

--- part4/scripts/measure-cpu-utilisation.sh
+++ part4/scripts/measure-cpu-utilisation.sh
@@ -1,38 +1,55 @@
 output_file=$1
 
 # Initialize the CSV file with headers
-echo "Time (s),instantaneous CPU Usage (%), cummulative CPU Usage (%)" > "$output_file"
+#echo "Time (s),instantaneous CPU Usage (%), cummulative CPU Usage (%)" > "$output_file"
+
+
 
 # Initialize previous values
-prev_total=0
-prev_idle=0
+num_cpus=8
+
+for ((i=0; i<num_cpus; i++)); do
+    prev_total[i]=0
+    prev_idle[i]=0
+    inst_cpu_usage[i]=0
+done
+
 
 # Infinite loop to monitor CPU usage
 while true; do
     # Read the first line of /proc/stat
     time=$(($(date +%s%N)/1000000))
-    read -r cpu user nice system idle iowait irq softirq steal guest guest_nice < /proc/stat
 
-    # cumulative CPU usage
-    total=$((user + nice + system + idle + iowait + irq + softirq + steal))
-    used=$(((total - idle)))
-    cum_cpu_usage=$(echo "scale=3; 100*$used / $total" | bc)
+    for ((i=0; i<num_cpus; i++)); do
+
+      line=$(head -n $((i+1)) /proc/stat | tail -n 1)
 
 
-    # instantaneous CPU usage
-    diff_total=$((total - prev_total))
-    diff_idle=$((idle - prev_idle))
-    diff_used=$((diff_total - diff_idle))
-    inst_cpu_usage=$(echo "scale=3; 100*$diff_used / $diff_total" | bc)
+      read -r cpu user nice system idle iowait irq softirq steal guest guest_nice <<< "$line"
 
+      # cumulative CPU usage
+      total=$((user + nice + system + idle + iowait + irq + softirq + steal))
+      used=$(((total - idle)))
 
+      # instantaneous CPU usage
+      diff_total=$((total - prev_total[i]))
+      diff_idle=$((idle - prev_idle[i]))
+      diff_used=$((diff_total - diff_idle))
+      inst_cpu_usage[i]=$(echo "scale=3; 100*$diff_used / $diff_total" | bc)
+
+      # Update previous values
+      prev_total[i]=$total
+      prev_idle[i]=$idle
+    done
     # Log the time and CPU usage to the CSV file
-    echo "$time, $inst_cpu_usage, $cum_cpu_usage" >> "$output_file"
+    output="$time"
+    for ((i=0; i<num_cpus; i++)); do
+        output+=", ${inst_cpu_usage[$i]}"
+    done
 
-    # Update previous values
-    prev_total=$total
-    prev_idle=$idle
+    echo "$output" >> "$output_file"
+    echo "$output"
 
     # Wait for 5 seconds before the next iteration
-    sleep 5
+    sleep 1
 done
