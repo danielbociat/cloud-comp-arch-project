438b611e1fd671d32d75897814cb1817a135ee61
cblack389 <negruclaudiu389@yahoo.com>
2025-05-10 17:40:29 +0200

    Fix controller

--- controller.py
+++ controller.py
@@ -69,6 +69,12 @@ class Controller:
         }
 
 
+    def pull_images(self):
+        for job_name in self.container_info:
+            print(f"pulling image {self.container_info[job_name]['image']}")
+            self.docker_client.images.pull(self.container_info[job_name]["image"])
+
+
     def _get_main_pid(self, service_name):
         try:
             result = subprocess.run(
@@ -118,9 +124,8 @@ class Controller:
 
     def get_containers_on_core(self, core):
         result = list()
-        for container in self.docker_client.containers.list(all=True):
-        #for key, value in self.container_info:
-            if core in container.attrs['HostConfig'].get('CpusetCpus', ''):
+        for container in self.docker_client.containers.list(filters={"status": "running"}):
+            if str(core) in container.attrs['HostConfig'].get('CpusetCpus', ''):
                 print(container.attrs['HostConfig'].get('CpusetCpus', ''))
                 result.append(container.name)
         return result
@@ -129,15 +134,15 @@ class Controller:
     # should only include running containers
     def get_containers_on_corei_not_corej(self, core1, core2):
         result = list()
-        for container in self.docker_client.containers.list(all=True):
-            if core1 in container.attrs['HostConfig'].get('CpusetCpus', '') and not core2 in container.attrs['HostConfig'].get('CpusetCpus', ''):
+        for container in self.docker_client.containers.list(filters={"status": "running"}):
+            if str(core1) in container.attrs['HostConfig'].get('CpusetCpus', '') and not str(core2) in container.attrs['HostConfig'].get('CpusetCpus', ''):
                 print(container.attrs['HostConfig'].get('CpusetCpus', ''))
                 result.append(container.name)
         return result
 
 
     # should update self.finished
-    def gather_finished_container(self):
+    def gather_finished_containers(self):
         result = list()
         for container in self.docker_client.containers.list(all=True, filters={"status": "exited"}):
             result.append(container.name)
@@ -168,14 +173,24 @@ class Controller:
     
     def add_core(self, container, core):
         new_cores = self.docker_client.containers.get(container).attrs['HostConfig'].get('CpusetCpus', '')
-        if core not in new_cores:
+        if str(core) not in new_cores:
             new_cores = f"{new_cores},{core}"
-        self.docker_client.containers.list(all=True, name=container).update(cpuset_cpus=f"{new_cores}")
+        self.docker_client.containers.get(container).update(cpuset_cpus=f"{new_cores}")
 
 
-    def get_memcached_resource_usage(self) -> list:
+    def get_per_core_cpu_usage(self) -> list:
         cpu_per_core = psutil.cpu_percent(percpu=True)
+        time.sleep(1)
+        cpu_per_core = psutil.cpu_percent(percpu=True) # twice because first call is unreliable
         return cpu_per_core
+    
+
+    def get_memcached_cpu_usage(self) -> float:
+        cpu_per_core = self.get_per_core_cpu_usage()
+        if self.memcached_num_cores == 1:
+            return cpu_per_core[0]
+        else: # memcached has 2 cores: 0,1
+            return cpu_per_core[0] + cpu_per_core[1]
 
         
     def create_container(self, image: str, job_name: str, cpuset_cpus: str, num_threads: int):
@@ -204,23 +219,31 @@ class Controller:
     
 
     def update_container(self, job_name: str, cpuset_cpus: str):
-        self.container_info[job_name]["obj"].update(cpuset_cpus=cpuset_cpus)
+        self.docker_client.containers.get(job_name).update(cpuset_cpus=cpuset_cpus)
 
-    
+
     def pause_container(self, job_name: str):
-        self.container_info[job_name]["obj"].pause()
+        print(f"pausing container: {job_name}")
+        self.docker_client.containers.get(job_name).pause()
+
+
+    def pause_containers(self, containers: list[str]):
+        for container_name in containers:
+            self.pause_container(container_name)
 
     
     def unpause_container(self, job_name: str):
-        self.container_info[job_name]["obj"].unpause()        
+        self.docker_client.containers.get(job_name).unpause()        
 
 
     def schedule(self):
+        self.pull_images()
         start_time = time.time()
         self.create_all_containers()
 
         while len(self.finished) < 7:
-            mcd_cpu = self.get_memcached_cpu()
+            time.sleep(1)
+            mcd_cpu = self.get_memcached_cpu_usage()
             if self.memcached_num_cores == 1 and mcd_cpu > self.T_mcd_1core:
                 self.expand_memcached_to_2_cores()
             elif self.memcached_num_cores == 2 and mcd_cpu < self.T_mcd_2core_low:
@@ -230,12 +253,13 @@ class Controller:
                 for container in containers_core_1:
                     self.pause_container(container)
 
-            cpu_utilisation = self.get_memcached_resource_usage() # assume it works per core
+            cpu_utilisation = self.get_per_core_cpu_usage() # assume it works per core
+            print(f"cpu usage: {cpu_utilisation}")
             if cpu_utilisation[2] < self.T1_cpu: # low utilisation on core 2, schedule more
                 self.schedule_next_job("2")
             elif cpu_utilisation[2] < self.T2_cpu: # medium utilisation, go to core 3
                 if cpu_utilisation[3] < self.T1_cpu:
-                    self.schedule_next_job("2")
+                    self.schedule_next_job("3")
                 elif cpu_utilisation[3] < self.T2_cpu:
                     pass # should maybe schedule sth on 1?
                 else: # need to move to 1
@@ -245,16 +269,20 @@ class Controller:
                             self.add_core(conts[0], 1)
                         else:
                             containers3 = self.get_containers_on_core(3)
-                            self.pause_container(containers3)
+                            if containers3:
+                                self.pause_container(containers3[0])
                     else:
                         containers3 = self.get_containers_on_core(3)
-                        self.pause_container(containers3)
+                        if containers3:
+                            self.pause_container(containers3[0])
 
             else: # high utilisation, add other cores for containers or pause something
                 if cpu_utilisation[3] < self.T1_cpu: # add core 3 for some container running on core 2
                     conts = self.get_containers_on_corei_not_corej(2, 3)
-                    if len(conts) != 0:
+                    if len(conts) != 0: 
                         self.add_core(conts[0], 3)
+                    else: # TODO: logic
+                        pass
                 elif cpu_utilisation[3] < self.T2_cpu: # 3 has a balanced utilisation, try to move on 1
                     if cpu_utilisation[1] < self.T1_cpu:
                         conts = self.get_containers_on_corei_not_corej(2, 1)
@@ -262,10 +290,12 @@ class Controller:
                             self.add_core(conts[0], 1)
                         else:
                             containers2 = self.get_containers_on_core(2)
-                            self.pause_container(containers2)
+                            if containers2:
+                                self.pause_container(containers2[0])
                     else:
                         containers2 = self.get_containers_on_core(2)
-                        self.pause_container(containers2)
+                        if containers2:
+                            self.pause_container(containers2[0])
                 else: # both 2 and 3 have high utilisation
                     containers1 = self.get_containers_on_core(1)
                     containers2 = self.get_containers_on_core(2)
@@ -279,14 +309,18 @@ class Controller:
                                 self.add_core(movable_conts[0], 1)
                         # if nothing shared can be moved to 1, move sth from c2, stop sth on c3
                         else:
-                            self.add_core(containers2, 1)
-                            self.pause_container(containers3)
+                            self.add_core(containers2[0], 1)
+                            if containers3:
+                                self.pause_container(containers3[0])
                     else: # can't move to 1
-                        if len(containers23) != 0:
+                        if containers23:
                             self.pause_container(containers23[0])
                         else:
-                            self.pause_container(containers2[0])
-                            self.pause_container(containers3[0])
+                            print(cpu_utilisation)
+                            if containers2:
+                                self.pause_container(containers2[0])
+                            if containers3:
+                                self.pause_container(containers3[0])
 
 
             self.gather_finished_containers()
